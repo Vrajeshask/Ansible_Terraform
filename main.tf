9cc96a03c0a736d9ede390dcebcab123168a7a04
Vrajesh Patel <patelvrajeshazure@gmail.com>
2024-04-07 22:59:31 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -67,5 +67,5 @@ resource "azurerm_linux_virtual_machine" "ncpl-vm" {
 }
 
 output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.ncpl-vm.network_interface_ids[0].ip_configuration[0].public_ip_address
+  value = azurerm_linux_virtual_machine.ncpl-vm.public_ip_address
 }
