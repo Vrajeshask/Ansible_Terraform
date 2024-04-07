fbadb27450b33ba2032e29154cb02d8f78978de0
Vrajesh Patel <patelvrajeshazure@gmail.com>
2024-04-07 02:14:52 +0000

    Added variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "resource_group_name" {
+    description = "Name of Resource Group"
+    type = string
+}
+
+variable "location" {
+    description = "Name of location"
+    type = string
+}
+
+variable "nic_name" {
+  description = "Name of the network interface "
+  type = string
+}
+
+variable "vm_name" {
+    description = " name of vm"
+    type = string
+}
+
+
+variable "vn_name" {
+    description = " name of Virtual network"
+    type = string
+}
