10d12b888d8ae3ea0b1f2323d28e5e5a7f558fe6
Vrajesh Patel <patelvrajeshazure@gmail.com>
2024-04-08 01:19:08 +0000

    Updated custom_data.sh

--- custom_data.sh
+++ custom_data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Add a new user
-sudo useradd -m -s /bin/bash patelvrajeshazure
+sudo useradd -m -s /bin/bash vrajesh
 
 # Set a password for the new user
-echo "patelvrajeshazure:newpassword" | sudo chpasswd
+echo "vrajesh:newpassword" | sudo chpasswd
 
 # Create SSH directory for the new user
-sudo mkdir -p /home/patelvrajeshazure/.ssh
+sudo mkdir -p /home/vrajesh/.ssh
 
 # Copy your public SSH key to the new user's authorized_keys file
-sudo cp /home/adminuser/.ssh/authorized_keys /home/patelvrajeshazure/.ssh/
-sudo chown patelvrajeshazure:patelvrajeshazure /home/patelvrajeshazure/.ssh/authorized_keys
-sudo chmod 600 /home/patelvrajeshazure/.ssh/authorized_keys
+sudo cp /home/adminuser/.ssh/authorized_keys /home/vrajesh/.ssh/
+sudo chown vrajesh:vrajesh /home/vrajesh/.ssh/authorized_keys
+sudo chmod 600 /home/vrajesh/.ssh/authorized_keys
