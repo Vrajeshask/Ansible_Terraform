2bf7cb392f074a7f4d384448bb2e692fa3e32aa1
Vrajesh Patel <patelvrajeshazure@gmail.com>
2024-04-10 03:35:23 +0000

    Updated inventory.py

--- inventory.py
+++ inventory.py
@@ -1,20 +1,19 @@
 import json
 
-def add_host_to_inventory(ip_address, ansible_user, inventory_file):
-    with open(inventory_file, 'a') as f:
-        f.write(f"{ip_address} ansible_user={ansible_user}\n")
-
 # Path to the Terraform output JSON file
 terraform_output_file = "/home/patelvrajeshazure/myagent/_work/1/s/terraform_output.json"
 
-# Read the Terraform output JSON file and extract the IP address
+# Read and parse JSON content from the Terraform output file
 with open(terraform_output_file, 'r') as f:
     terraform_output = json.load(f)
-    ip_address = terraform_output["vm_ip_address"]
 
-# Assigning other variables
+# Extract the IP address from the parsed JSON
+ip_address = terraform_output["vm_ip_address"]
+
+# Assign other variables as needed
 ansible_user = "adminuser"
-inventory_file = "/home/patelvrajeshazure/myagent/_work/1/s/inventory.ini"
+inventory_file = "inventory.ini"
 
-# Call the function to add the host to the inventory
-add_host_to_inventory(ip_address, ansible_user, inventory_file)
+# Add host to inventory file
+with open(inventory_file, 'a') as f:
+    f.write(f"{ip_address} ansible_user={ansible_user}\n")
